9053fa5e767485da1570ff39409a0a0eb0e9c206
nathan <nathanmageka6@gmail.com>
2023-06-04 22:22:49 +0300

    nathan

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |attribute, value|
+      self.class.send(:attr_accessor, attribute)
+      instance_variable_set("@#{attribute}", value)
+    end
 end
